c2fd0f164fc38262b8000930e6989f4c05eb8410
D7702_10 <d7702_10@d7702-10ui-imac.local>
2019-04-03 16:45:15 +0900

    UITextField Test 01

--- UITextField Test 01/ViewController.swift
+++ UITextField Test 01/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Delegate class connection
+        myTextField.delegate = self
+        
+        // 텍스트 박스 지우는 용도
+        myTextField.clearButtonMode = UITextField.ViewMode.always
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        let inString = myTextField.text
+        myLabel.text = inString
+        myTextField.text = ""
+        
+        // 키패드 내림 ( 권한 박탈 )
+        myTextField.resignFirstResponder()
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        myTextField.resignFirstResponder()
+        return true
+    }
+    
 }
 
